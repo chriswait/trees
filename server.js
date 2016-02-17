fb8cfe81710e81ad6c55d595bb1f1fdd37e60970
chriswait <chriswait91@gmail.com>
2016-02-17 23:23:11 +1100

    refactors getting species for marker

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 var data_url = "http://trees.cityofsydney.nsw.gov.au/map/";
 var data_path = './data/data.json';
 
+var trim = function(input) {
+    return input.replace(/^\s+|\s+$/g, "");
+};
+
 var fetch_data = function() {
     console.log("Fetching data");
     request(data_url, function(error, response, html) {
@@ -27,57 +31,35 @@ var fetch_data = function() {
 var process_data = function(marker_data) {
     console.log("Processing data");
 
-    function trim(input) {
-        return input.replace(/^\s+|\s+$/g, "");
-    };
-
-    function get_species_list(tree_species_text) {
-        var species_strings = [];
-        if (tree_species_text.indexOf(';') == -1) {
-            species_strings = [tree_species_text];
-        } else {
-            var species_list = tree_species_text.split(';');
-            for (var i=0; i<species_list.length; i++) {
-                var trimmed = trim(species_list[i]);
-                if (trimmed) species_strings.push(trimmed);
-            }
-        }
-
-        var species_list = [];
-        for (var i=0; i<species_strings.length; i++) {
-            var species = species_strings[i];
-            var species_count = species.substr(0, species.indexOf(' '));
-            var species_name = species.substr(species.indexOf(' ')+1);
-            species_list.push({
-                name: species_name,
-                count: species_count,
-            });
-        }
-
-        return species_list;
-    };
-
     var markers = [];
+    var all_species = [];
+
     for (var i=0; i < marker_data.length; i++) {
         var marker = marker_data[i];
         if (typeof marker.custom_fields.species != "undefined" && marker.custom_fields.species) {
             var tree_species_text = trim(marker.custom_fields.species.split(":")[1]);
-            var tree_species_list = get_species_list(tree_species_text);
+
+            var species_list = [];
+            var species_text_list = tree_species_text.split(';');
+            for (var j=0; j<species_text_list.length; j++) {
+                var species = trim(species_text_list[j]);
+                if (species) {
+                    var species_count = species.substr(0, species.indexOf(' '));
+                    var species_name = species.substr(species.indexOf(' ')+1);
+                    if (all_species.indexOf(species_name) == -1) all_species.push(species_name);
+                    species_list.push({
+                        name: species_name,
+                        count: species_count,
+                    });
+                }
+            }
+
             markers.push({
                 "title": marker.title,
                 "lat": marker.custom_fields.lat,
                 "lng": marker.custom_fields.lng,
-                "species": tree_species_list,
+                "species": species_list,
             });
-        }
-    }
-
-    var all_species = [];
-    for (var i=0; i< markers.length; i++) {
-        var species = markers[i].species;
-        for (var j=0; j < species.length; j++) {
-            var species_name = species[j].name;
-            if (all_species.indexOf(species_name) == -1) all_species.push(species_name);
         }
     }
 
