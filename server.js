e88278d4d79f597ad8e659a89c1b5772676d4747
chriswait <chriswait91@gmail.com>
2016-02-17 23:09:50 +1100

    stores lists of species objects, trees->markers

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-var tree_data_path = './data/tree_data.json';
-var trees_url = "http://trees.cityofsydney.nsw.gov.au/map/";
+var data_url = "http://trees.cityofsydney.nsw.gov.au/map/";
+var data_path = './data/data.json';
 
-var get_data = function() {
-    console.log("Fetching tree data");
-    request(trees_url, function(error, response, html) {
+var fetch_data = function() {
+    console.log("Fetching data");
+    request(data_url, function(error, response, html) {
         if (!error) {
-            console.log("Scraping tree data");
+            console.log("Scraping data");
             var $ = cheerio.load(html);
-            var tree_data_js = $("#maincontent").children()[0].children[0].data;
-            var tree_data_list_text = tree_data_js.split("= ")[1].slice(0, -2);
-            var tree_data = JSON.parse('{"trees": ' + tree_data_list_text + '}');
-            process_data(tree_data);
+            var data_js = $("#maincontent").children()[0].children[0].data;
+            var data_list_text = data_js.split("= ")[1].slice(0, -2);
+            var marker_data = JSON.parse(data_list_text);
+            process_data(marker_data);
         } else {
             console.log("Error fetching data:");
             console.log(error);
@@ -24,73 +24,84 @@ var get_data = function() {
     });
 };
 
-var process_data = function(tree_data) {
-        console.log("Processing tree data");
+var process_data = function(marker_data) {
+    console.log("Processing data");
 
-        function trim(input) {
-            return input.replace(/^\s+|\s+$/g, "");
-        };
+    function trim(input) {
+        return input.replace(/^\s+|\s+$/g, "");
+    };
 
-        function get_species_list(tree_species_text) {
-            var tree_species_list = [];
-            if (tree_species_text.indexOf(';') == -1) {
-                tree_species_list = [tree_species_text];
-            } else {
-                var species_list = tree_species_text.split(';');
-                for (j=0; j<species_list.length; j++) {
-                    var trimmed = trim(species_list[j]);
-                    if (trimmed) tree_species_list.push(trimmed);
-                }
-            }
-            return tree_species_list;
-        };
-
-        var trees = [];
-        for (var i=0; i < tree_data.trees.length; i++) {
-            var tree = tree_data.trees[i];
-            if (typeof tree.custom_fields.species != "undefined" && tree.custom_fields.species) {
-                var tree = tree_data.trees[i];
-                var tree_species_text = trim(tree_data.trees[i].custom_fields.species.split(":")[1]);
-                var tree_species_list = get_species_list(tree_species_text);
-                trees.push({
-                    "title": tree.title,
-                    "lat": tree.custom_fields.lat,
-                    "lng": tree.custom_fields.lng,
-                    "species": tree_species_list,
-                });
+    function get_species_list(tree_species_text) {
+        var species_strings = [];
+        if (tree_species_text.indexOf(';') == -1) {
+            species_strings = [tree_species_text];
+        } else {
+            var species_list = tree_species_text.split(';');
+            for (var i=0; i<species_list.length; i++) {
+                var trimmed = trim(species_list[i]);
+                if (trimmed) species_strings.push(trimmed);
             }
         }
 
-        var all_species = [];
-        for (var i=0; i< trees.length; i++) {
-            var species = trees[i].species;
-            for (var j=0; j < species.length; j++) {
-                var species_name = species[j].substr(species[j].indexOf(' ')+1);
-                if (all_species.indexOf(species_name) == -1) all_species.push(species_name);
-            }
+        var species_list = [];
+        for (var i=0; i<species_strings.length; i++) {
+            var species = species_strings[i];
+            var species_count = species.substr(0, species.indexOf(' '));
+            var species_name = species.substr(species.indexOf(' ')+1);
+            species_list.push({
+                name: species_name,
+                count: species_count,
+            });
         }
 
-        var data = {
-                trees: trees,
-                species: all_species,
-        };
-        var output = JSON.stringify(data, null, 2);
+        return species_list;
+    };
 
-        console.log("Storing tree data");
-        fs.writeFile(tree_data_path, output, function(err) {
-            if (err) return console.log(err);
-        });
-        console.log("Done!");
+    var markers = [];
+    for (var i=0; i < marker_data.length; i++) {
+        var marker = marker_data[i];
+        if (typeof marker.custom_fields.species != "undefined" && marker.custom_fields.species) {
+            var tree_species_text = trim(marker.custom_fields.species.split(":")[1]);
+            var tree_species_list = get_species_list(tree_species_text);
+            markers.push({
+                "title": marker.title,
+                "lat": marker.custom_fields.lat,
+                "lng": marker.custom_fields.lng,
+                "species": tree_species_list,
+            });
+        }
+    }
+
+    var all_species = [];
+    for (var i=0; i< markers.length; i++) {
+        var species = markers[i].species;
+        for (var j=0; j < species.length; j++) {
+            var species_name = species[j].name;
+            if (all_species.indexOf(species_name) == -1) all_species.push(species_name);
+        }
+    }
+
+    var data = {
+            markers: markers,
+            species: all_species,
+    };
+    var output = JSON.stringify(data, null, 2);
+
+    console.log("Storing data");
+    fs.writeFile(data_path, output, function(err) {
+        if (err) return console.log(err);
+    });
+    console.log("Done!");
 };
 
-app.get('/trees', function(req, res) {
-    var tree_data = require(tree_data_path);
-    var response = JSON.stringify(tree_data, null, 2);
+app.get('/data', function(req, res) {
+    var data = require(data_path);
+    var response = JSON.stringify(data, null, 2);
     res.setHeader('Content-Type', 'application/json');
     res.send(response);
 });
 
-get_data();
+fetch_data();
 
 console.log("Serving application");
 app.listen('8082');
