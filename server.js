afb12015095920e2beacf27b02305726cdcf8bd7
chriswait <chriswait91@gmail.com>
2016-02-20 18:14:15 +1100

    fetches extra info html, promises
    adds bluebird to package.json
    some half-finished refactoring...

--- server.js
+++ server.js
@@ -1,79 +1,131 @@
+var Promise = require('bluebird');
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-var data_url = "http://trees.cityofsydney.nsw.gov.au/map/";
+var site_root = "http://trees.cityofsydney.nsw.gov.au/";
+var data_url = site_root + "map/";
+var location_url_root = site_root + "location/";
+
+var raw_path = "./data/raw.json"
 var data_path = './data/data.json';
+
+var markers = [];
+var all_species = [];
 
 var trim = function(input) {
     return input.replace(/^\s+|\s+$/g, "");
 };
 
-var fetch_data = function() {
-    console.log("Fetching data");
-    request(data_url, function(error, response, html) {
-        if (!error) {
-            console.log("Scraping data");
+var fetch_marker_data = function() {
+    console.log("FETCHING");
+    return new Promise(function(resolve, reject) {
+        request(data_url, function(error, response, html) {
+            if (error) {
+                console.log("Error fetching data:");
+                console.log(error);
+                return reject(error);
+            }
             var $ = cheerio.load(html);
             var data_js = $("#maincontent").children()[0].children[0].data;
             var data_list_text = data_js.split("= ")[1].slice(0, -2);
+
+            fs.writeFile(raw_path, data_list_text, function(err) {
+                if (err) return console.log(err);
+            });
+
             var marker_data = JSON.parse(data_list_text);
-            process_data(marker_data);
-        } else {
-            console.log("Error fetching data:");
-            console.log(error);
-        }
+            resolve(marker_data);
+        });
     });
 };
 
-var process_data = function(marker_data) {
-    console.log("Processing data");
+var fetch_marker_information = function(marker_slug) {
+    console.log("FETCHING MARKER INFO");
+    return new Promise(function(resolve, reject) {
+        var url = location_url_root + marker_slug + "/";
+        request(data_url, function(error, response, html) {
+            if (error) {
+                console.log("Error fetching data:");
+                console.log(error);
+                return reject(error);
+            }
+            var $ = cheerio.load(html);
+            console.log("FETCHED EXTRA");
+            resolve(html);
+        });
+    });
+};
 
-    var markers = [];
-    var all_species = [];
+var process_marker = function(marker) {
+    console.log("PROCESSING MARKER")
+    return new Promise(function(resolve, reject) {
+        var new_marker = {
+            "title": marker.title,
+            "lat": marker.custom_fields.lat,
+            "lng": marker.custom_fields.lng,
+            "slug": marker.slug
+        };
 
-    for (var i=0; i < marker_data.length; i++) {
-        var marker = marker_data[i];
+        // Get species
+        var species_list = [];
         if (typeof marker.custom_fields.species != "undefined" && marker.custom_fields.species) {
             var tree_species_text = trim(marker.custom_fields.species.split(":")[1]);
-
-            var species_list = [];
             var species_text_list = tree_species_text.split(';');
             for (var j=0; j<species_text_list.length; j++) {
                 var species = trim(species_text_list[j]);
                 if (species) {
-                    var species_count = species.substr(0, species.indexOf(' '));
                     var species_name = species.substr(species.indexOf(' ')+1);
-                    if (all_species.indexOf(species_name) == -1) all_species.push(species_name);
+                    var species_count = species.substr(0, species.indexOf(' '));
                     species_list.push({
                         name: species_name,
-                        count: species_count,
+                        count: species_count
                     });
+                    if (all_species.indexOf(species_name) == -1) {
+                        all_species.push(species_name);
+                        console.log("ADDING SPECIES: " + species.length);
+                    }
                 }
             }
-
-            markers.push({
-                "title": marker.title,
-                "lat": marker.custom_fields.lat,
-                "lng": marker.custom_fields.lng,
-                "species": species_list,
-            });
         }
-    }
+        new_marker.species = species_list;
 
+        fetch_marker_information(marker.slug)
+        .then(function(html) {
+            new_marker.html = html;
+            markers.push(new_marker);
+            console.log("ADDING MARKER: " + markers.length);
+            resolve(new_marker);
+        });
+    });
+};
+
+var process_marker_data = function(marker_data) {
+    console.log("PROCESSING MARKERS")
+    var marker_promises = [];
+    //for (var i=0; i < marker_data.length; i++) {
+    for (var i=0; i < 6; i++) {
+        var marker = marker_data[i];
+        var marker_promise = process_marker(marker);
+        marker_promises.push(marker_promise);
+    }
+    return Promise.all(marker_promises);
+};
+
+var write_data = function() {
+    console.log("WRITING DATA");
+    console.log("markers: " + markers.length);
+    console.log("species: " + all_species.length);
     var data = {
             markers: markers,
-            species: all_species,
+            species: all_species
     };
     var output = JSON.stringify(data, null, 2);
-
-    console.log("Storing data");
     fs.writeFile(data_path, output, function(err) {
         if (err) return console.log(err);
     });
-    console.log("Done!");
 };
 
 app.get('/data', function(req, res) {
@@ -83,8 +135,12 @@ app.get('/data', function(req, res) {
     res.send(response);
 });
 
-fetch_data();
-
-console.log("Serving application");
-app.listen('8082');
-exports = module.exports = app;
+fetch_marker_data()
+.then(function(marker_data) {
+    process_marker_data(marker_data)
+    .then(function() {
+        write_data();
+        app.listen('8082');
+        exports = module.exports = app;
+    });
+})
